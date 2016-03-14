3d0abd88f3a560a5f0552d69aec465d8f1f97c9e
Allan Hortle <allanhortle@gmail.com>
2016-03-14 21:09:08 +1100

    Clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,15 +10,13 @@
 
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
+    grunt.registerTask('test', ['clean', 'cssflow', 'nodeunit']);
+    grunt.registerTask('default', ['cssflow']);
 
-        // Before generating any new files, remove any previously-created files.
+    grunt.initConfig({
         clean: {
             tests: ['tmp']
         },
-
-        // Configuration to be run (and then tested).
         cssflow: {
             sass: {
                 options :{
@@ -39,27 +37,14 @@ module.exports = function(grunt) {
                 }
             }
         },
-
-        // Unit tests.
         nodeunit: {
             tests: ['test/*_test.js']
         }
-
     });
 
-    // Actually load this plugin's task(s).
     grunt.loadTasks('tasks');
-
-    // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'cssflow', 'nodeunit']);
-
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['cssflow']);
 
 };
